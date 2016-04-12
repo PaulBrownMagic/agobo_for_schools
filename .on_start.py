de1b8464af7f623b397601655fdd740075b28850
Paul Brown <pbrownpaul@gmail.com>
2016-04-12 16:01:23 +0100

    Added morse IP flash and stopped double running
    Added in a feature that will flash the last section of the IP address (eg. 192.168.1.155 takes just 155) in binary. This is to ensure the IP address is what you expect it to be if the VNC server is running and pupils still can't log in. It is assumed that only the last section of the IP is critical. The number will flash only a nibble if the number is less than 16, otherwise it will flash a whole byte. 
    
    Also added in a check against a bool in a file to ensure the program only runs once, otherwise it'll mess up the morse!

--- .on_start.py
+++ .on_start.py
@@ -1,73 +1,127 @@
 #! /usr/bin/python3
 
-import RPi.GPIO as GPIO
-from time import sleep
-from subprocess import check_output, call
-GPIO.setmode(GPIO.BCM)
-RED = 4
-GREEN = 17
-GPIO.setup(RED, GPIO.OUT, initial = 1)
-GPIO.setup(GREEN, GPIO.OUT, initial = 0)
+# File to ensure only one copy runs at a time, .profile can be called twice.
+with open('.running','r') as file:
+    is_running = list(file)[0].strip() == 'True'
+    print(is_running)
 
-def has_ip():
-    return len(check_output(["hostname", "-I"]).decode('utf-8').strip()) > 6
+if not is_running:
+    print("Started")
+    with open('.running', 'w') as file:
+        file.write('True')
+    import RPi.GPIO as GPIO
+    from time import sleep
+    from subprocess import check_output, call
+    GPIO.setmode(GPIO.BCM)
+    RED = 4
+    GREEN = 17
+    GPIO.setup(RED, GPIO.OUT, initial = 1)
+    GPIO.setup(GREEN, GPIO.OUT, initial = 0)
 
-def has_license():
-    return len(check_output(["cat", "/etc/vnc/licensekey"]).decode('utf-8').strip()) > 15
+    def get_output(command):
+        return check_output(command).decode('utf-8').strip()
 
-def vncserver_started():
-    return '1.pid' in subprocess.check_output(["ls", ".vnc"]).decode('utf-8').strip()
+    def has_ip():
+        return len(get_output(["hostname", "-I"])) > 6 
 
-red, green = False, True
+    def has_license():
+        return len(get_output(["cat", "/etc/vnc/licensekey"])) > 15
 
-# Get IP address
-for _ in range(10):
-    red = not red
-    GPIO.output(RED, red)
-    sleep(0.03)
-while not has_ip():
-    red = not red
-    GPIO.output(RED, red)
-    sleep(0.1)
+    def vncserver_started():
+        if '0.pid' in get_output(["ls", ".vnc"]):
+            process_name = get_output(['cat','.vnc/gui:0.pid'])
+            ps_name = get_output(['ps', '-p', process_name, '-o', 'comm='])
+            if 'vnc' in ps_name.lower():
+                return True
+        return False
 
-GPIO.output(RED, 0)
-GPIO.output(GREEN, 1)
+    red, green = False, True
 
-# Assert License
-for _ in range(10):
-    red = not red
-    green = not green
-    GPIO.output(RED, red)
-    GPIO.output(GREEN, green)
-    sleep(0.03)
-while not has_license():
-    red = not red
-    green = not green
-    GPIO.output(RED, red)
-    GPIO.output(GREEN, green)
-    call(["sudo","vnclicense","-add", "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"])  # You'll need your own key here, they are free
-    sleep(0.1)
+    # Get IP address -- Flash red
+    for _ in range(16):
+        red = not red
+        GPIO.output(RED, red)
+        sleep(0.03)
+    while not has_ip():
+        red = not red
+        GPIO.output(RED, red)
+        sleep(0.2)
 
-GPIO.output(RED, 0)
-GPIO.output(GREEN, 1)
+    GPIO.output(RED, 0)
+    GPIO.output(GREEN, 1)
 
-# Start VNC server
-for _ in range(10):
-    green = not green
-    GPIO.output(GREEN, green)
-    sleep(0.03)
-call(["vncserver",":1"])
-while not vncserver_started:
-    call(["vncserver",":1"])
-    green = not green
-    GPIO.output(GREEN, green)
-    sleep(0.1)
+    # Assert License -- Flash red/green
+    for _ in range(16):
+        red = not red
+        green = not green
+        GPIO.output(RED, red)
+        GPIO.output(GREEN, green)
+        sleep(0.05)
+    while not has_license():
+        red = not red
+        green = not green
+        GPIO.output(RED, red)
+        GPIO.output(GREEN, green)
+        call(["sudo","vnclicense","-add", "DAHR2-D2DSQ-BQ3A4-74SJV-KU3WA"])
+        sleep(0.2)
 
-GPIO.output(RED, 0)
-GPIO.output(GREEN, 1)
+    GPIO.output(RED, 0)
+    GPIO.output(GREEN, 1)
 
-# Leave Green light on
-sleep(30)
+    # Start VNC server -- Flash green
+    call(["vncserver",":!"])
+    for _ in range(16):
+        green = not green
+        GPIO.output(GREEN, green)
+        sleep(0.05)
+    server_running = vncserver_started()
+    while not server_running:
+        call(["vncserver",":!"])
+        green = not green
+        GPIO.output(GREEN, green)
+        sleep(0.1)
+        server_running = vncserver_started()
 
-# Exit
-GPIO.cleanup()
+    GPIO.output(RED, 0)
+    GPIO.output(GREEN, 1)
+
+    # Leave Green light on, morse flash last IP element.
+    def get_morse():
+        ip_last_bin = bin(int(get_output(["hostname", "-I"]).split(".")[-1]))
+        morse = list(map(int,list(ip_last_bin)[2:]))
+        if len(morse) <= 4:
+            while len(morse) < 4:
+                morse = [0] + morse
+        elif len(morse) < 8:
+            while len(morse) < 8:
+                morse = [0] + morse
+        return morse
+
+    def has_connection():
+        log = get_output(['tail', '.vnc/gui:0.log'])
+        return 'connected' in log
+
+    sleep(2)
+    morse = get_morse()
+    connected = False
+    dot = 0.3
+    dash = dot * 3
+
+    while not connected:
+        connected = has_connection()
+        sleep(1)
+        for signal in morse:
+            GPIO.output(RED, True)
+            if signal == 0:
+                sleep(dot)
+            else:
+                sleep(dash)
+            GPIO.output(RED, False)
+            sleep(dot)
+    sleep(1)
+
+    # Exit
+    GPIO.cleanup()
+
+with open('.running', 'w') as file:
+    file.write('False')
